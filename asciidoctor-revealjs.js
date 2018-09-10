4b1b986c78640faee0caf2db78d2ce3fa1aeddf0
Alex Li <chengdong.li@walmartlabs.com>
2018-09-09 17:09:25 -0700

    Make asciidoctor-revealjs.js respect the path of revealjs(locally). Also add a subfolder to demo the presentation in subfolder

--- asciidoctor-revealjs.js
+++ asciidoctor-revealjs.js
@@ -7,15 +7,29 @@ if(args.length<=2) {
 }
 
 var adoc = args[2];
-console.log(adoc);
+console.log('name='+adoc);
+console.log('cwd='+process.cwd())
+console.log('cmd='+__dirname)
 
+function findPrefix() {
+  var cur=process.cwd().split("/")
+  var cmd=__dirname.split("/")
+  var i, prefix=""
+  for (i=cmd; i<cur; i++) {
+    prefix += "../"
+  }
+
+  console.log("prefix="+prefix)
+  return prefix;
+}
 
 // Load asciidoctor.js and asciidoctor-reveal.js
 var asciidoctor = require('asciidoctor.js')();
 require('asciidoctor-reveal.js');
 
 // Convert the document 'presentation.adoc' using the reveal.js converter
-var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
+// var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
+var attributes = {'revealjsdir': findPrefix()+'node_modules/reveal.js@'};
 //var attributes = {'revealjsdir': 'https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0'};
 var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
 asciidoctor.convertFile(adoc, options);
